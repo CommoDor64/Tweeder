18a97c5c4a5f8698c4bcb63783d65b527b9083a1
Dor Cohen <commo64dor@gmail.com>
2019-02-11 15:18:47 +0100

    config changed to rhea servers, done (:

--- app/src/config/web3.config.js
+++ app/src/config/web3.config.js
@@ -4,5 +4,5 @@ const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 const abi = Tweeder_artifacts.abi;
 const address = Tweeder_artifacts.networks[1234].address;
 const Tweeder = new web3.eth.Contract(abi, address);
-web3.eth.defaultAccount = '0x66fbe4d04fd97925259433bc6bc11985fb536edf';
+web3.eth.defaultAccount = '0xBBcFBDCAA798a68156190228D4E7311Bb00733e1';
 export { Tweeder, web3 };
